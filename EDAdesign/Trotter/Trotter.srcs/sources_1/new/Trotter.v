c69f8495d4cd9365bb5a3475a205d5435fa50215
Noyie05 <1418884542@qq.com>
2023-06-07 11:41:38 +0800

    Debug

--- EDAdesign/Trotter/Trotter.srcs/sources_1/new/Trotter.v
+++ EDAdesign/Trotter/Trotter.srcs/sources_1/new/Trotter.v
@@ -25,28 +25,31 @@ module Trotter(
 
     output [7:0] L 
     );
-    reg [1:0] sel;
+
     reg repeat_2;
     reg [7:0] repeat_7;
     reg [31:0] count;
     reg [7:0] Y;
-
-    parameter model_1=2'b00,
+    reg[1:0] sel;
+    
+    parameter 
+              model_1=2'b00,
               model_2=2'b01,
               model_3=2'b10,
               model_4=2'b11;
-
+              
     parameter Idle =8'b11111111,
               Special=8'b11110000;
 
-    always @(posedge sys_clk) 
-        begin
+
+    always @(posedge sys_clk or negedge rst_n) 
             if(!rst_n)
              begin
-                sel<=S;
                 repeat_2<=0;
-                repeat_7<=0;  
+                repeat_7<=0;
+                count<=0;  
                 Y<=Idle; 
+                sel<=S;
              end
             else 
              case(sel)
@@ -113,130 +116,8 @@ module Trotter(
 
                          end
                     end
+                  default :sel<=model_1;
              endcase
-        end
 
-assign L=Y;
-
-task task_counter;
-    inout  task_count;
-    inout  task_rep;
-     begin
-            if(task_count >= 32'd1_000_000_000)
-                begin
-                 task_rep <= task_rep+1;
-                 task_count <= 32'd0;
-                end
-            else
-                begin
-                 task_rep <= task_rep;
-                 task_count <= task_count + 32'd1;
-                 end
-     end
-endtask
-
-task my_task_model1;
-    inout  task_count;
-    inout [7:0] task_Y;
-    inout [3:0] repeat_count;
-    
-    begin
-      if(repeat_count==15)
-        begin
-            task_counter(task_count,repeat_count);
-            task_Y[15-repeat_count]<=task_Y[15-repeat_count]^1;   
-            repeat_count<=0;
-            task_Y<=Idle;
-
-        end
-      else if(repeat_count>=8) //当repeat_count==8时 task_Y=8’b00000000
-        begin
-        task_counter(task_count,repeat_count);
-        task_Y[15-repeat_count]<=task_Y[15-repeat_count]^1;
-        end
-      else
-        begin
-        task_counter(task_count,repeat_count);
-        task_Y[7-repeat_count]<=task_Y[7-repeat_count]^1;
-        end
-    end 
-endtask
-
-task my_task_model2;//前面四个灯为7~4 后面四个灯为3~0
-    inout  task_count;
-    inout [7:0] task_Y;
-    input [3:0] repeat_count;
-    
-    begin
-      if(repeat_count==8)
-         begin
-            task_counter(task_count,repeat_count);
-            task_Y[11-repeat_count]<=task_Y[11-repeat_count]^1;
-            task_Y[repeat_count-4]<=task_Y[repeat_count-4]^1;
-            repeat_count<=0;
-         end
-      else if(repeat_count>=4) //当repeat_count==4时 task_Y=8’b00000000
-        begin
-        task_counter(task_count,repeat_count);
-        task_Y[11-repeat_count]<=task_Y[11-repeat_count]^1;
-        task_Y[repeat_count-4]<=task_Y[repeat_count-4]^1;
-        end
-      else
-        begin
-        task_counter(task_count,repeat_count);
-        task_Y[7-repeat_count]<=task_Y[7-repeat_count]^1;
-        task_Y[repeat_count]<=task_Y[repeat_count]^1;
-        end
-    end 
-endtask
-
-task my_task_model3;//前面四个灯为7~4 后面四个灯为3~0
-    inout  task_count;
-    inout [7:0] task_Y;//默认输入是11110000
-    input [3:0] repeat_count;//默认输入是0
-    
-    begin
-      if(repeat_count==15) 
-       begin
-         task_counter(task_count,repeat_count);
-         task_Y<=8'b11110000;
-        end
-      else if(repeat_count>7) 
-         begin
-          task_counter(task_count,repeat_count);
-          task_Y[15-repeat_count]<=task_Y[15-repeat_count]^1;
-         end
-      else  
-         begin
-          task_counter(task_count,repeat_count);
-          task_Y[7-repeat_count]<=task_Y[7-repeat_count]^1;
-         end
-    end 
-endtask
-
-task my_task_model4;//流水灯
-    inout  task_count;
-    inout [7:0] task_Y;//默认输入是11111111
-    input [3:0] repeat_count;//默认输入是0
-    
-    begin
-      task_Y<=Idle;
-      if(repeat_count==15) 
-       begin
-         task_counter(task_count,repeat_count);
-         task_Y<=Idle;
-        end
-      else if(repeat_count>7) 
-         begin
-          task_counter(task_count,repeat_count);
-          task_Y[15-repeat_count]<=task_Y[15-repeat_count]^1;
-         end
-      else  
-         begin
-          task_counter(task_count,repeat_count);
-          task_Y[7-repeat_count]<=task_Y[7-repeat_count]^1;
-         end
-    end 
-endtask
-
+   assign L=Y;
 endmodule
--- EDAdesign/Trotter/Trotter.srcs/sources_1/new/Trotter_top.v
+++ EDAdesign/Trotter/Trotter.srcs/sources_1/new/Trotter_top.v
@@ -26,24 +26,152 @@ module Trotter_top(
 
     reg sys_clk;
     reg rst_n;
-    reg [1:0] sel;
+    reg [1:0] sys_sel;
 
     wire [7:0] Y_out;
 
+    
+    parameter Idle =8'b11111111,
+              Special=8'b11110000;
+
     always #1 sys_clk=~sys_clk;
-    always #10000000000 rst_n=~rst_n;
+
 
     initial
      begin
          sys_clk=0;
-         rst_n=1;
-         #10 rst_n=rst_n;
+         rst_n=0;
+         sys_sel<=2'bxx;
+         #10 rst_n=~rst_n;
+         #20 sys_sel=~sys_sel;
      end
 
      Trotter Trotter(
         .sys_clk(sys_clk),
         .rst_n(rst_n),
-        .S(sel),
-        .Y_out(L)
+        .S(sys_sel),
+        .L(Y_out)
      );
+    
+    
+task automatic task_counter;
+    inout  task_count;
+    inout  task_rep;
+     begin
+            if(task_count == 32'd1_000)
+                begin
+                 task_rep = task_rep+1;
+                 task_count = 32'd0;
+                end
+            else
+                begin
+                 task_rep = task_rep;
+                 task_count = task_count + 32'd1;
+                 end
+     end
+endtask
+
+task my_task_model1;
+    inout  task_count;
+    inout [7:0] task_Y;
+    inout [3:0] repeat_count;
+    
+    begin
+      if(repeat_count==15)
+        begin
+            task_counter(task_count,repeat_count);
+            task_Y[15-repeat_count]=task_Y[15-repeat_count]^1;   
+            repeat_count=0;
+            task_Y=Idle;
+
+        end
+      else if(repeat_count>=8) //当repeat_count==8时 task_Y=8’b00000000
+        begin
+        task_counter(task_count,repeat_count);
+        task_Y[15-repeat_count]=task_Y[15-repeat_count]^1;
+        end
+      else
+        begin
+        task_counter(task_count,repeat_count);
+        task_Y[7-repeat_count]=task_Y[7-repeat_count]^1;
+        end
+    end 
+endtask
+
+task my_task_model2;//前面四个灯为7~4 后面四个灯为3~0
+    inout  task_count;
+    inout [7:0] task_Y;
+    input [3:0] repeat_count;
+    
+    begin
+      if(repeat_count==8)
+         begin
+            task_counter(task_count,repeat_count);
+            task_Y[11-repeat_count]=task_Y[11-repeat_count]^1;
+            task_Y[repeat_count-4]=task_Y[repeat_count-4]^1;
+            repeat_count=0;
+         end
+      else if(repeat_count>=4) //当repeat_count==4时 task_Y=8’b00000000
+        begin
+        task_counter(task_count,repeat_count);
+        task_Y[11-repeat_count]=task_Y[11-repeat_count]^1;
+        task_Y[repeat_count-4]=task_Y[repeat_count-4]^1;
+        end
+      else
+        begin
+        task_counter(task_count,repeat_count);
+        task_Y[7-repeat_count]=task_Y[7-repeat_count]^1;
+        task_Y[repeat_count]=task_Y[repeat_count]^1;
+        end
+    end 
+endtask
+
+task my_task_model3;//前面四个灯为7~4 后面四个灯为3~0
+    inout  task_count;
+    inout [7:0] task_Y;//默认输入是11110000
+    input [3:0] repeat_count;//默认输入是0
+    
+    begin
+      if(repeat_count==15) 
+       begin
+         task_counter(task_count,repeat_count);
+         task_Y=8'b11110000;
+        end
+      else if(repeat_count>7) 
+         begin
+          task_counter(task_count,repeat_count);
+          task_Y[15-repeat_count]=task_Y[15-repeat_count]^1;
+         end
+      else  
+         begin
+          task_counter(task_count,repeat_count);
+          task_Y[7-repeat_count]=task_Y[7-repeat_count]^1;
+         end
+    end 
+endtask
+
+task my_task_model4;//流水灯
+    inout  task_count;
+    inout [7:0] task_Y;//默认输入是11111111
+    input [3:0] repeat_count;//默认输入是0
+    
+    begin
+      task_Y=Idle;
+      if(repeat_count==15) 
+       begin
+         task_counter(task_count,repeat_count);
+         task_Y=Idle;
+        end
+      else if(repeat_count>7) 
+         begin
+          task_counter(task_count,repeat_count);
+          task_Y[15-repeat_count]=task_Y[15-repeat_count]^1;
+         end
+      else  
+         begin
+          task_counter(task_count,repeat_count);
+          task_Y[7-repeat_count]=task_Y[7-repeat_count]^1;
+         end
+    end 
+endtask
 endmodule
