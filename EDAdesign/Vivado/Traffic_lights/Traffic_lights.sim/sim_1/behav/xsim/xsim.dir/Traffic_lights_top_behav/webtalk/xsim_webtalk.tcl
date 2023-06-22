7334570af67ed6e08192e77692e8813c10eba08e
Noyie05 <1418884542@qq.com>
2023-06-22 15:39:27 +0800

    一些修改

--- EDAdesign/Vivado/Traffic_lights/Traffic_lights.sim/sim_1/behav/xsim/xsim.dir/Traffic_lights_top_behav/webtalk/xsim_webtalk.tcl
+++ EDAdesign/Vivado/Traffic_lights/Traffic_lights.sim/sim_1/behav/xsim/xsim.dir/Traffic_lights_top_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir D:/Project/FPGA/FPGA/EDAdesign/Vivado/Traffic_lights/Traffic_lights.sim/sim_1/behav/xsim/xsim.dir/Traffic_lights_top_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Thu Jun 22 13:35:49 2023" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Thu Jun 22 15:35:52 2023" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2019.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2708876" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "ba514fe5-9f58-4f4d-a65f-93efb1a51804" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "b05205ef5765422a8861796300214d70" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "53" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "54" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i7-10510U CPU @ 1.80GHz" -context "user_environment"
@@ -26,7 +26,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "120000100 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.06_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "9880_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 3926265190 -regid "" -xml D:/Project/FPGA/FPGA/EDAdesign/Vivado/Traffic_lights/Traffic_lights.sim/sim_1/behav/xsim/xsim.dir/Traffic_lights_top_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/Project/FPGA/FPGA/EDAdesign/Vivado/Traffic_lights/Traffic_lights.sim/sim_1/behav/xsim/xsim.dir/Traffic_lights_top_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/Project/FPGA/FPGA/EDAdesign/Vivado/Traffic_lights/Traffic_lights.sim/sim_1/behav/xsim/xsim.dir/Traffic_lights_top_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.09_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "9816_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 2553160508 -regid "" -xml D:/Project/FPGA/FPGA/EDAdesign/Vivado/Traffic_lights/Traffic_lights.sim/sim_1/behav/xsim/xsim.dir/Traffic_lights_top_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/Project/FPGA/FPGA/EDAdesign/Vivado/Traffic_lights/Traffic_lights.sim/sim_1/behav/xsim/xsim.dir/Traffic_lights_top_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/Project/FPGA/FPGA/EDAdesign/Vivado/Traffic_lights/Traffic_lights.sim/sim_1/behav/xsim/xsim.dir/Traffic_lights_top_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
